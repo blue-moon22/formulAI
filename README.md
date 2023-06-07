b18a7665f4fd91d3b6511c05da32b607747a6f55
Victoria Carr <vc11@sanger.ac.uk>
2023-06-07 21:02:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# FormulAI
+
+LangChain/GPT 3.5 App for generating ingredients and protocols for formulations
+
+## Prerequisites
+
+Get an [OpenAI API Key](https://platform.openai.com/account/api-keys) and set it as your environmental variable `OPENAI_API_KEY`
+
+## Install
+
+```
+git clone https://github.com/blue-moon22/formulAI.git
+cd formulAI
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Quickstart
+
+```
+streamlit run app.py
+```
+
+## To Do
+
+- Connect with PatentsView API
+- Search for claims from particular cosmetic/consumer healthcare companies
+- Prompt refinement
+
+## Resources
+
+- [LangChain Crash Course: Build a AutoGPT app in 25 minutes!](https://www.youtube.com/watch?v=MlK6SIjcjE8)
---
+++ app.py
@@ -0,0 +1,51 @@
+import os
+import streamlit as st
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
+
+# App framework
+st.title("🪄🧪 FormulAI")
+prompt = st.text_input("What would you like to formulate?")
+
+# Prompt templates
+ingredients_template = PromptTemplate(
+    input_variables = ['chemical'],
+    template = "Write me the ingredients for a formulation that includes {chemical}."
+)
+
+protocol_template = PromptTemplate(
+    input_variables = ['ingredients', 'wikipedia_research'],
+    template = "Write a protocol for putting together the {ingredients} while leveraging this wikipedia reserch:{wikipedia_research}."
+)
+
+# Memory 
+ingredients_memory = ConversationBufferMemory(input_key='chemical', memory_key='chat_history')
+protocol_memory = ConversationBufferMemory(input_key='ingredients', memory_key='chat_history')
+
+# LLMs
+llm = OpenAI(model_name="gpt-3.5-turbo")
+ingredients_chain = LLMChain(llm=llm, prompt=ingredients_template, verbose=True, output_key="ingredients", memory=ingredients_memory)
+protocol_chain = LLMChain(llm=llm, prompt=protocol_template, verbose=True, output_key="protocol", memory=protocol_memory)
+
+wiki = WikipediaAPIWrapper()
+
+# Show stuff on screen
+if prompt:
+    ingredients = ingredients_chain.run(prompt)
+    wiki_research = wiki.run(prompt) 
+    protocol = protocol_chain.run(ingredients=ingredients, wikipedia_research=wiki_research)
+
+    st.write(ingredients)
+    st.write(protocol) 
+
+    with st.expander('Ingredients History'): 
+        st.info(ingredients_memory.buffer)
+
+    with st.expander('Protocol History'): 
+        st.info(protocol_memory.buffer)
+
+    with st.expander('Wikipedia Research'): 
+        st.info(wiki_research)
